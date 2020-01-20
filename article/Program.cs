ff88fc3df243c90265ae2845350edc286e2fa3ad
Ridwan Nugroho <ridwan.nugroho@bsi.co.id>
2020-01-20 12:49:55 +0700

    refactor article

--- article/Program.cs
+++ article/Program.cs
@@ -17,80 +17,144 @@ namespace article
             string json = r.ReadToEnd();
 
             List<User> items = JsonConvert.DeserializeObject<List<User>>(json);
+            
+            separator("Users who does not have phone number : ");
+            foreach (var user in DontHvPhone(items))
+                Console.WriteLine(user);
 
-            Console.WriteLine("Users who does not have phone number : ");
+            separator("Users who have articles : ");
+            foreach (var user in HaveArticle(items))
+                Console.WriteLine(user);
+
+            separator("Users who have \"annis\": ");
+            foreach (var user in FindName(items, "annis"))
+                Console.WriteLine(user);
+
+            separator("Users who have article on 2020");
+            foreach (var user in AuthorArticleOn(items, 2020))
+                Console.WriteLine(user);
+            
+            separator("Users who are born on 1986");
+            foreach (var user in FindNameByBirthday(items, 1986))
+                Console.WriteLine(user);
+
+            separator("Article that contain \"tips\" on the title: ");
+            foreach (var item in FindArtContain(items, "tips"))
+                Console.WriteLine("{0} : {1}", item[0], item[1]);
+
+            separator("Article published before August 2019: ");
+            DateTime th = DateTime.Parse("August 2019");
+            foreach (var item in FindArtPublishedBfr(items, "August 2019"))
+                Console.WriteLine("{0} : {1}", item[0], item[1]);
+
+            separator("~~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~~~");
+        }
+
+
+        static void separator( string info)
+        {
+            Console.WriteLine();
+            Console.WriteLine("==========================================");
+            Console.WriteLine(info);
+        }
+    
+        static List<string> DontHvPhone(List<User> items)
+        {
+            var userFullName = new List<string>();
             foreach (var user in items)
             {
                 if(user.Profile.Phones.Length == 0)
-                    Console.WriteLine(user.Profile.full_name);
+                    userFullName.Add(user.Profile.full_name);
             }
 
-            Console.WriteLine();
-            Console.WriteLine("==========================================");
-            Console.WriteLine("Users who does not have articles : ");
+            return userFullName;
+        }
+
+        static List<string> HaveArticle(List<User> items)
+        {
+            var userFullName = new List<string>();
             foreach (var user in items)
             {
                 if(user.articles.Count > 0)
-                    Console.WriteLine(user.Profile.full_name);
+                    userFullName.Add(user.Profile.full_name);
             }
 
-            Console.WriteLine();
-            Console.WriteLine("==========================================");
-            Console.WriteLine("Users who does not have \"annis\": ");
+            return userFullName;
+        }
+    
+        static List<string> FindName(List<User> items, string name)
+        {
+            var userFullName = new List<string>();
             foreach (var user in items)
             {
                 if(user.Profile.full_name.IndexOf("annis", 0, StringComparison.CurrentCultureIgnoreCase) != -1)
-                    Console.WriteLine(user.Profile.full_name);
+                    userFullName.Add(user.Profile.full_name);
             }
 
-            Console.WriteLine();
-            Console.WriteLine("==========================================");
-            Console.WriteLine("Users who have article on 2020");
+            return userFullName;
+        }
+
+        static List<string> AuthorArticleOn(List<User> items, int year)
+        {
+            var userFullName = new List<string>();
             foreach (var user in items)
             {
                 foreach (var art in user.articles)
                 {
-                    if(art.GetArticleYear() == 2019)
+                    if(art.GetArticleYear() == year && !userFullName.Contains(user.Profile.full_name))
                     {
-                        Console.WriteLine(user.Profile.full_name);
+                        userFullName.Add(user.Profile.full_name);
                         break;
                     }
                 }
             }
-            
-            Console.WriteLine();
-            Console.WriteLine("==========================================");
-            Console.WriteLine("Users who are born on 1986");
+
+            return userFullName;
+        }
+    
+        static List<string> FindNameByBirthday(List<User> items, int year)
+        {
+            var userFullName = new List<string>();
             foreach (var user in items)
             {
-                if(user.Profile.GetBornYear() == 1986)
-                    Console.WriteLine(user.Profile.full_name);
+                if(user.Profile.GetBornYear() == year)
+                    userFullName.Add(user.Profile.full_name);
             }
 
-            Console.WriteLine();
-            Console.WriteLine("==========================================");
-            Console.WriteLine("Article that contain \"tips\" on the title: ");
+            return userFullName;
+        }
+    
+        static List<List<string>> FindArtContain(List<User> items, string find)
+        {
+            var articles = new List<List<string>>();
+
             foreach (var user in items)
             {
                 foreach (var art in user.articles)
                 {
-                    if(art.Title.IndexOf("tips", 0, StringComparison.CurrentCultureIgnoreCase) != -1)
-                        Console.WriteLine("{0} , oleh : {1}", art.Title, user.Profile.full_name);
+                    if(art.Title.IndexOf(find, 0, StringComparison.CurrentCultureIgnoreCase) != -1)
+                        articles.Add(new List<string> {art.Title, user.Profile.full_name});
                 }
             }
 
-            Console.WriteLine();
-            Console.WriteLine("==========================================");
-            Console.WriteLine("Article published before August 2019: ");
-            DateTime th = DateTime.Parse("August 2019");
+            return articles;
+        }
+
+        static List<List<string>> FindArtPublishedBfr(List<User> items, string date)
+        {
+            DateTime th = DateTime.Parse(date);
+            var articles = new List<List<string>>();
+
             foreach (var user in items)
             {
                 foreach (var art in user.articles)
                 {
                     if(art.published_at < th)
-                        Console.WriteLine("{0} , oleh : {1}", art.Title, user.Profile.full_name);
+                        articles.Add(new List<string> {art.Title, user.Profile.full_name});
                 }
             }
+
+            return articles;
         }
     }
     
