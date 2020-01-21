bcd985391b9f995cf8bb8fd65e09f354e172bb64
Ridwan Nugroho <ridwan.nugroho@bsi.co.id>
2020-01-21 11:05:57 +0700

    las commit before move to LINQ branch

--- inv/InvParser.cs
+++ inv/InvParser.cs
@@ -17,7 +17,6 @@ namespace InvParser
         {
             DateTime dt = new DateTime(1970,1,1,0,0,0,0,DateTimeKind.Utc);
             dt = dt.AddSeconds(purchased_at).ToLocalTime();
-
             return dt;
         }
 
